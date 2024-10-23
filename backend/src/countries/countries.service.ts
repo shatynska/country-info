8f15cd68196dace442fff17346ecc0ca4311000a
Helen Shatynska <helen.shatynska@gmail.com>
2024-10-23 15:30:25 +0300

    feat: add getAll controller & service

--- backend/src/countries/countries.service.ts
+++ backend/src/countries/countries.service.ts
@@ -1,7 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { CountryBriefInfoDto } from './dto/country-brief-info.dto';
 
 @Injectable()
 export class CountriesService {
   constructor(private readonly configService: ConfigService) {}
+
+  async getAll(): Promise<CountryBriefInfoDto[]> {
+    const fetchCountriesData = async () => {
+      try {
+        const url =
+          this.configService.get('DATE_NAGER_API_BASE_URL') +
+          'AvailableCountries';
+
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error('Failed to fetch counties data');
+        }
+        return await response.json();
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    const countries = await fetchCountriesData();
+
+    return countries;
+  }
 }
